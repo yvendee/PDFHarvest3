8c32e3df4e4e1071601d898f6a279a4551dfbf4b
yvendee <71810479+yvendee@users.noreply.github.com>
2026-01-18 14:20:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ import cv2
 import numpy as np
 from PIL import Image
 from flask_cors import CORS
-from openai_api.utils.utils import ( get_summary_from_image, get_summary_from_text, get_summary_from_text_gpt4o, get_summary_from_text_test, get_summary_from_text_gpt4omini, get_summary_from_image_gpt4omini, detect_face_gpt4omini)
+from openai_api.utils.utils import ( get_summary_from_image, get_summary_from_text, get_summary_from_text_gpt4o, get_summary_from_text_test, get_summary_from_text_gpt4omini, get_summary_from_image_gpt4omini, detect_face_gpt4omini, get_summary_from_text_gpt5mini, get_summary_from_text_gpt5nano, get_summary_from_image_gpt5nano, get_summary_from_image_gpt5mini)
 
 from anthropic_api.utils.utils import ( get_summary_from_image_using_claude )
 
@@ -2694,6 +2694,7 @@ def download_logs():
 if __name__ == '__main__':
     app.run(debug=True)
     app.run(host='0.0.0.0', port=3000)
+
 
 
 
